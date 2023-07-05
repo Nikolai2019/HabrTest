924e08e73f85f0e4fe7978012e39e81f17c32329
Nikolai2019 <kolya.ruslyakov@gmail.com>
2023-07-05 14:15:24 +0500

    supportHeaderTest and sendButtonSupportTest tests created

--- src/test/java/com/example/habrtest/MainPageTest.java
+++ src/test/java/com/example/habrtest/MainPageTest.java
@@ -11,7 +11,9 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
 import java.time.Duration;
+import java.util.List;
 
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class MainPageTest {
     private WebDriver driver;
 
@@ -33,6 +35,7 @@ public class MainPageTest {
     }
 
     @Test
+    @Order(1)
     public void changeLogTest() {
 
         WebElement userIcon = driver.findElement(By.cssSelector("svg[data-test-id='menu-toggle-guest']"));
@@ -41,7 +44,31 @@ public class MainPageTest {
         WebElement rulesLink = driver.findElement(By.xpath("//*[contains(text(),'Правила сайта')]"));
         rulesLink.click();
 
-        assertTrue(driver.findElement(By.xpath("//*[contains(text(),'Changelog')]")).isDisplayed(), "CHANGELOG не найден");
+        List<WebElement> changeLog = (driver.findElements(By.xpath("//*[contains(text(),'Changelog')]")));
+        assertFalse(changeLog.isEmpty(), "CHANGELOG не найден");
     }
 
+    @Test
+    @DisplayName("Проверка заголовка Техническая поддержка")
+    @Order(2)
+    public void supportHeaderTest(){
+        WebElement supportButton = driver.findElement(By.xpath("//*[contains(text(),'Техническая поддержка')]"));
+        supportButton.click();
+
+        WebElement mainSupportText = driver.findElement(By.xpath("//h1[contains(text(),'Техническая поддержка')]"));
+        assertTrue(mainSupportText.isDisplayed(),"Секция отсутствует");
+        System.out.println(mainSupportText);
+    }
+
+    @Test
+    @DisplayName("Проверка наличия кнопки Отправить на форме обращения")
+    @Order(3)
+    public void sendButtonSupportTest(){
+        WebElement supportButton = driver.findElement(By.xpath("//*[contains(text(),'Техническая поддержка')]"));
+        supportButton.click();
+
+        WebElement submitButton = driver.findElement(By.cssSelector("button[class='tm-form__submit base-button']"));
+        assertTrue(submitButton.isDisplayed(),"Кнопка отсутствует");
+        System.out.println(submitButton);
+    }
 }
